b10e7cde64026c3d74790c12e3348c1b5edd2a0c
Anderson <105141966+andersonjhonatan@users.noreply.github.com>
2023-02-02 20:19:58 -0300

    2 - Requisito

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,24 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
-class Header extends Component {
-  render() {
-    return (
-      <div>Header</div>
-    );
-  }
+function Header({ ValorDoMapState }) {
+  return (
+    <div>
+      <h2 data-testid="email-field">
+        {ValorDoMapState}
+      </h2>
+      <p data-testid="total-field">0</p>
+      <p data-testid="header-currency-field">BRL</p>
+    </div>
+  );
 }
+const mapStateToProps = ({ user: { email } }) => ({
+  ValorDoMapState: email,
+});
 
-export default Header;
+export default connect(mapStateToProps)(Header);
+
+Header.propTypes = {
+  ValorDoMapState: PropTypes.string.isRequired,
+};
